be4fd16005796524406d99d60a7b2d29ba952440
Marcin Pietrzyk <mlppietrzyk@gmail.com>
2018-04-17 22:33:46 +0200

    5th commit timer.js

---
+++ timer.js
@@ -0,0 +1,18 @@
+function counting()
+	{
+		var today = new Date();
+			
+
+		var hour = today.getHours();
+		if (hour<10) hour = "0" + hour;
+		
+		var minutes = today.getMinutes();
+		if (minutes<10) minutes = "0"+minutes;
+		
+		var seconds = today.getSeconds();
+		if (seconds<10) seconds = "0"+seconds;
+			
+		document.getElementById("clock").innerHTML = hour+":"+minutes+":"+seconds;
+			
+		setTimeout("counting()",1000);
+	}
